f6ce95aa572d0c01c1e95c95d38e647a406bbdc5
Maxim Korsakov <maks.korsakov.2017@mail.ru>
2024-01-07 10:50:55 +0200

    created post pets api test

--- src/support/random-generator.ts
+++ src/support/random-generator.ts
@@ -7,4 +7,9 @@ export default class RandomGenerator {
         this.generatedUsername = config.credentials.username + Math.floor(Math.random() * 1000).toString().padStart(3, '0');
         return this.generatedUsername;
     }
+    static generateRandomNumber(): number {
+        const defaultMin = 1;
+        const defaultMax = 1000;
+        return Math.floor(Math.random() * (defaultMax - defaultMin + 1)) + defaultMin;
+    }
 }
